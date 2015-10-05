b10d8f98a49918642efaa88b02b486cbbd4fc8ac
Stefan <ilie.stefan92@yahoo.com>
2015-10-05 15:00:40 +0300

    hello world

--- PropertyFinder/index.ios.js
+++ PropertyFinder/index.ios.js
@@ -1,2 +1,20 @@
 'use strict';
 var React = require('react-native');
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render(){
+    return React.createElement(React.text, {style: styles.text}, "Hello World!");
+  }
+}
+
+React.AppRegistry.registerComponent(
+  'PropertyFinder', function() {return PropertyFinderApp}
+);
