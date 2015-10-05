60f22d4ae9b88eb78dd6e6b486e0748825b4d890
Stefan <ilie.stefan92@yahoo.com>
2015-10-05 17:03:30 +0300

    first view

--- PropertyFinder/index.ios.js
+++ PropertyFinder/index.ios.js
@@ -1,17 +1,28 @@
 'use strict';
 var React = require('react-native');
+var SearchPage = require('./SearchPage');
 var styles = React.StyleSheet.create({
   text: {
     color: 'black',
     backgroundColor: 'white',
     fontSize: 30,
-    margin: 80
+    margin: 100
+  },
+  container: {
+    flex: 1
   }
 });
 
 class PropertyFinderApp extends React.Component {
   render() {
-    return React.createElement(React.Text, {style: styles.text}, "Hello World!");
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: "Property Finder",
+          component: SearchPage
+        }}/>
+    );
   }
 }
 
